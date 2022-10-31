81c49b2d24b0ce1fa1083371aef2cb33f594187a
elgatodelovecraft <112429511+elgatodelovecraft@users.noreply.github.com>
2022-10-31 08:11:36 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 input.onPinPressed(TouchPin.P0, function () {
-    if (Jugador_1 >= Jugador_2 && Jugador_1 <= 21) {
-    	
-    } else if (Jugador_2 >= Jugador_1 && Jugador_2 <= 21) {
+    if (Jugador_1 > Jugador_2 && Jugador_1 <= 21) {
+        win_1()
+    }
+    if (Jugador_2 > Jugador_1 && Jugador_2 <= 21) {
     	
     }
 })
@@ -11,10 +12,12 @@ input.onButtonPressed(Button.A, function () {
 })
 function lose () {
     if (Jugador_1 > 21) {
-        basic.showString("You lose")
+        basic.showString("Jugador 1 loses")
+        basic.clearScreen()
     }
     if (Jugador_2 > 21) {
-        basic.showString("You lose")
+        basic.showString("Jugador 2 loses")
+        basic.clearScreen()
     }
 }
 input.onButtonPressed(Button.AB, function () {
@@ -22,10 +25,20 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(1500)
     basic.clearScreen()
 })
+function win_1 () {
+    basic.showNumber(Jugador_1)
+    basic.showString("Congratulations Jugador 1")
+    game.gameOver()
+}
 input.onButtonPressed(Button.B, function () {
     Jugador_2 = Jugador_2 + randint(1, 11)
     basic.showNumber(Jugador_2)
 })
+function win_2 () {
+    basic.showNumber(Jugador_2)
+    basic.showString("Congratulations Jugador 2")
+    game.gameOver()
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showNumber(Jugador_1)
     basic.pause(1500)
@@ -36,5 +49,5 @@ let Jugador_1 = 0
 Jugador_1 = randint(2, 21)
 Jugador_2 = randint(2, 21)
 basic.forever(function () {
-	
+    lose()
 })
